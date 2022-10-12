401b799b860773d61b2a3b3c678d3db42fd7331d
tuandat333111 <tuandattuandat123@gmail.com>
2022-10-12 15:59:55 +0700

    initilizone excersise

---
+++ main.js
@@ -0,0 +1,12 @@
+// Bài tập 1 
+// Tính tiền lương nhân viên
+// Mô hình 3 khối:
+// 1. Nhập giá trị đầu vào:
+"use strict";
+const ps = require("prompt-sync");
+const prompt = ps();
+let n = prompt("Enter n: "); //n là số ngày làm
+// 2. Xử kí giá trị:
+let ketqua=100000*n;
+// 3. In kết quả:
+console(ketqua,ketqua)
